45c2346509b99d3506a2aa216cdc986e8ebb2d5b
ZAYROX <226544310+juliantecher84-bot@users.noreply.github.com>
2025-10-19 17:25:20 +0200

    Update fxmanifest.lua

--- ZayroxCarCommande/fxmanifest.lua
+++ ZayroxCarCommande/fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'ZAYROX, discord : https://discord.gg/xwJ4jaqKGY'
-description 'Commande /car pour faire spawn le véhicule de votre choix supplément on spawn dans le véhicule discord : https://discord.gg/xwJ4jaqKGY'
+author 'ZAYROX'
+description 'Commande /car pour faire spawn le véhicule de votre choix supplément on spawn dans le véhicule'
 version '1.0.0'
 
 server_script 'server.lua'
+
 client_script 'client.lua'
